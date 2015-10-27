2363854f9b18fa19b7a00420578ee7acddde039f
Cebrian <Cebrian@Cebrian-PC>
2015-10-27 13:16:36 +0100

    Actualizada Practica IPO

--- IPO_Practica2/src/_interface/CardLayoutExample.java
+++ IPO_Practica2/src/_interface/CardLayoutExample.java
@@ -21,12 +21,15 @@ public class CardLayoutExample {
 	private JButton btnPanel2;
 	private JButton btnPanel3;
 	private JPanel pnlContenido;
-	private JPanel panel_1;
 	private JPanel panel_2;
 	private JPanel panel_3;
-	private JLabel lblAlien;
 	private JLabel lblGato;
 	private JLabel lblHombreDeNegro;
+	private JLabel imgGato;
+	private JPanel panel_1;
+	private JLabel imgHombre;
+	private JLabel lblAlien;
+	private JLabel imgAlien;
 
 	/**
 	 * Launch the application.
@@ -62,7 +65,7 @@ public class CardLayoutExample {
 		panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.SOUTH);
 		
-		btnPanel1 = new JButton("Panel1");
+		btnPanel1 = new JButton("Panel 1");
 		btnPanel1.addActionListener(new BtnPanelActionListener());
 		panel.add(btnPanel1);
 		
@@ -79,26 +82,43 @@ public class CardLayoutExample {
 		pnlContenido.setLayout(new CardLayout(0, 0));
 		
 		panel_1 = new JPanel();
-		pnlContenido.add(panel_1, "Panel_1");
+		pnlContenido.add(panel_1, "Panel 1");
+		panel_1.setLayout(null);
 		
 		lblAlien = new JLabel("Alien");
-		lblAlien.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblAlien.setBounds(204, 122, 46, 14);
+		lblAlien.setBounds(198, 11, 33, 14);
 		panel_1.add(lblAlien);
 		
+		imgAlien = new JLabel("");
+		imgAlien.setIcon(new ImageIcon(CardLayoutExample.class.getResource("/_interface/019.png")));
+		imgAlien.setBounds(162, 36, 100, 107);
+		panel_1.add(imgAlien);
+		
 		panel_2 = new JPanel();
-		pnlContenido.add(panel_2, "Panel_2");
+		pnlContenido.add(panel_2, "Panel 2");
+		panel_2.setLayout(null);
 		
 		lblGato = new JLabel("Gato");
-		lblGato.setBounds(206, 116, 46, 14);
+		lblGato.setBounds(205, 5, 41, 14);
 		panel_2.add(lblGato);
 		
+		imgGato = new JLabel("");
+		imgGato.setIcon(new ImageIcon(CardLayoutExample.class.getResource("/_interface/022.png")));
+		imgGato.setBounds(169, 30, 100, 91);
+		panel_2.add(imgGato);
+		
 		panel_3 = new JPanel();
-		pnlContenido.add(panel_3, "Panel_3");
+		pnlContenido.add(panel_3, "Panel 3");
+		panel_3.setLayout(null);
 		
 		lblHombreDeNegro = new JLabel("Hombre de negro");
-		lblHombreDeNegro.setBounds(172, 139, 111, 14);
+		lblHombreDeNegro.setBounds(175, 5, 106, 14);
 		panel_3.add(lblHombreDeNegro);
+		
+		imgHombre = new JLabel("");
+		imgHombre.setIcon(new ImageIcon(CardLayoutExample.class.getResource("/_interface/431.png")));
+		imgHombre.setBounds(168, 30, 100, 115);
+		panel_3.add(imgHombre);
 	}
 	
 	private class BtnPanelActionListener implements ActionListener {
